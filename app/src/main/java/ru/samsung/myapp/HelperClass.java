08b35aa0cc40373cfe0bdf3db5c6efba552046c8
Davit <davitvahrami12@gmail.com>
2025-01-07 16:45:30 +0400

    Add files

---
+++ app/src/main/java/ru/samsung/myapp/HelperClass.java
@@ -0,0 +1,33 @@
+package ru.samsung.myapp;
+
+public class HelperClass {
+    String name, email, username, password;
+
+    public HelperClass() {
+        // Default constructor required for calls to DataSnapshot.getValue(HelperClass.class)
+    }
+
+    public HelperClass(String name, String email, String username, String password) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
